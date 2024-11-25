a64ba99a0fcd55570119d8b258095b831d841b17
Konrad Piszczek <kpiszczek67@gmail.com>
2024-11-25 16:33:45 +0100

    add device preview

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -1,3 +1,4 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../features/root/root_page.dart';
@@ -10,6 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'core/app.dart';
 import 'injection_container.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
   configureDependencies();
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const MyApp(), // Wrap your app
+    ),);
 }
 
 
