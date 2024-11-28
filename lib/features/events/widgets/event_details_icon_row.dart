ea02253ce4e82a3678edc2bc3919e10f10ae609e
Konrad Piszczek <kpiszczek67@gmail.com>
2024-11-28 19:06:13 +0100

    Refactor Icon Row in Event Detalis

--- lib/features/events/widgets/event_details_icon_row.dart
+++ lib/features/events/widgets/event_details_icon_row.dart
@@ -1,3 +1,4 @@
+import 'package:explore_app/features/events/pages/calendar_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -17,125 +18,83 @@ class EventDetailsIconRow extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Expanded(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: dw * .15,
-                  child: DecoratedBox(
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Color.fromRGBO(218, 231, 232, 1),
-                    ),
-                    child: IconButton(
-                      iconSize: dw * .1,
-                      onPressed: () {},
-                      icon: SvgPicture.asset(
-                          'assets/custom_icons/calendar_add.svg'),
-                    ),
-                  ),
+          EventDetailsIconButton(
+            dw: dw,
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => const CalendarPage(),
                 ),
-                const Text(
-                  'Dodaj do kalendarza',
-                  style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.w300,
-                    overflow: TextOverflow.clip,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ],
+              );
+            },
+            imgPath: 'assets/custom_icons/calendar_add.svg',
+            description: 'Dodaj do kalendarza',
+          ),
+          EventDetailsIconButton(
+            dw: dw,
+            onPressed: () {},
+            imgPath: 'assets/custom_icons/share.svg',
+            description: 'Udostępnij',
+          ),
+          EventDetailsIconButton(
+            dw: dw,
+            onPressed: () {},
+            imgPath: 'assets/custom_icons/navigate.svg',
+            description: 'Pokaż na mapie',
+          ),
+          EventDetailsIconButton(
+            dw: dw,
+            onPressed: () {},
+            imgPath: 'assets/custom_icons/www.svg',
+            description: 'Strona www',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class EventDetailsIconButton extends StatelessWidget {
+  const EventDetailsIconButton({
+    super.key,
+    required this.dw,
+    required this.onPressed,
+    required this.imgPath,
+    required this.description,
+  });
+
+  final double dw;
+  final Function() onPressed;
+  final String imgPath;
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        children: [
+          SizedBox(
+            height: dw * .15,
+            child: DecoratedBox(
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Color.fromRGBO(218, 231, 232, 1),
+              ),
+              child: IconButton(
+                iconSize: dw * .1,
+                onPressed: onPressed,
+                icon: SvgPicture.asset(imgPath),
+              ),
             ),
           ),
-          Expanded(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: dw * .15,
-                  child: DecoratedBox(
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Color.fromRGBO(218, 231, 232, 1),
-                    ),
-                    child: IconButton(
-                      iconSize: dw * .1,
-                      onPressed: () {},
-                      icon: SvgPicture.asset(
-                          'assets/custom_icons/share.svg'),
-                    ),
-                  ),
-                ),
-                const Text(
-                  'Udostępnij',
-                  style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.w300,
-                    overflow: TextOverflow.clip,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ],
+          Text(
+            description,
+            style: const TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w300,
+              overflow: TextOverflow.clip,
             ),
-          ),
-          Expanded(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: dw * .15,
-                  child: DecoratedBox(
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Color.fromRGBO(218, 231, 232, 1),
-                    ),
-                    child: IconButton(
-                      iconSize: dw * .1,
-                      onPressed: () {},
-                      icon: SvgPicture.asset(
-                          'assets/custom_icons/navigate.svg'),
-                    ),
-                  ),
-                ),
-                const Text(
-                  'Pokaż na mapie',
-                  style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.w300,
-                    overflow: TextOverflow.clip,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: dw * .15,
-                  child: DecoratedBox(
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Color.fromRGBO(218, 231, 232, 1),
-                    ),
-                    child: IconButton(
-                      iconSize: dw * .1,
-                      onPressed: () {},
-                      icon: SvgPicture.asset(
-                          'assets/custom_icons/www.svg'),
-                    ),
-                  ),
-                ),
-                const Text(
-                  'Strona www',
-                  style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.w300,
-                    overflow: TextOverflow.clip,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            ),
+            textAlign: TextAlign.center,
           ),
         ],
       ),
